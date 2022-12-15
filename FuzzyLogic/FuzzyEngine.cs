aca4ef7a727112a633a76ccfc9bfeaddff730bc5
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-12-16 00:01:00 +0800

    Add Fuzzy Logic System

--- FuzzyLogic/FuzzyEngine.cs
+++ FuzzyLogic/FuzzyEngine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FuzzyLogic
+namespace BacalsoControlPlayer.FuzzyLogic
 {
     public class FuzzyEngine
     {
