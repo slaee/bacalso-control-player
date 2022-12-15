f3a08a8ad2f0862e715b002172e8c0a4e01ac319
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-12-15 23:01:04 +0800

    Add Game Map class

---
+++ Utils/Map.cs
@@ -0,0 +1,18 @@
+using Skua.Core.Interfaces;
+using Skua.Core.Models;
+using Skua.Core.Models.Items;
+using Skua.Core.Models.Monsters;
+using Skua.Core.Models.Quests;
+using Skua.Core.Models.Servers;
+using Skua.Core.Models.Shops;
+using Skua.Core.Models.Skills;
+using Skua.Core.Options;
+using Skua.Core.Utils;
+
+namespace BacalsoControlPlayer.Utils
+{
+    public class Map
+    {
+        
+    }
+}
