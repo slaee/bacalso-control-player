0e7047a61f76c50c20b5743dc388bebdff4d515c
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-12-16 02:27:23 +0800

    Refactor and using fuzzy logic for HP and MP control

--- BotPlayer.cs
+++ BotPlayer.cs
@@ -43,6 +43,15 @@ public class BotPlayer
     private static BotPlayer _instance;
     public static BotPlayer Instance => _instance ??= new BotPlayer();
 
+    private FuzzyEngine FuzzyEngine = new();
+    private FuzzyRuleCollection FuzzyRules = new();
+    private LinguisticVariable _myHealth;
+    private LinguisticVariable _myMana;
+    private LinguisticVariable _myCondition;
+    private MembershipFunctionCollection Health = new();
+    private MembershipFunctionCollection Mana = new();
+    private MembershipFunctionCollection PlayerCondition = new();
+
     private Map Map = new();
     private Logger Log = new();
     private bool logEquip = true;
@@ -61,13 +70,16 @@ public class BotPlayer
 
     public void HuntForItem(string map, string item, int quantity)
     {
+        Log.Message($"Initializing Fuzzy Logic Engine");
+        SetMembers();
+        SetRules();
         Log.Message($"Hunting for {item} x{quantity} in {map}");
 
         this.EquipClass(ClassType.Farm);
         while (!Bot.ShouldExit && !CheckInventory(new string[] {item},quantity))
         {
-            this.KillMonster(map, "r2", "Left", "*", item, quantity);
-            this.KillMonster(map, "r3", "Left", "*", item, quantity);
+            this.KillMonster(map, "r2", "Bottom", "*", item, quantity);
+            this.KillMonster(map, "r3", "Bottom", "*", item, quantity);
         }
     }
 
@@ -95,18 +107,77 @@ public class BotPlayer
             int dynamicQuantity = Bot.Inventory.GetQuantity(item);
             Log.Message($"Killing {monster} for {item}, ({dynamicQuantity}/{quantity}) [Inventory = {item}]");
         }
-
+        
         while (!Bot.ShouldExit && !CheckInventory(item, quantity))
         {
             if (!Bot.Combat.StopAttacking)
             {
+                FuzzifyValues();
                 Bot.Combat.Attack(monster);
-
-                // TODO: Fuzzy Logic for Health and Mana Control
+                Defuzzy();
             }
-                
             Bot.Sleep(ActionDelay);
         }
+    }
+
+    private void FuzzifyValues()
+    {
+        _myHealth.InputValue = Convert.ToDouble(Bot.Player.Health);
+        _myHealth.Fuzzify("HIGH");
+        _myMana.InputValue = Convert.ToDouble(Bot.Player.Mana);
+        _myMana.Fuzzify("HIGH");
+    }
+
+    private void Defuzzy()
+    {
+        SetFuzzyEngine();
+        FuzzyEngine.Consequent = "PLAYERCONDITION";
+        Log.Message($"Defuzzified Value: {FuzzyEngine.Defuzzify()}");
+    }
+
+    private void SetMembers()
+    {   
+        int fullHealth = Bot.Player.Health;
+        int halfHealth = (fullHealth/2);
+        Health.Add(new MembershipFunction("LOW", 0.0, 150, (halfHealth-(halfHealth/1.5)), (halfHealth-(halfHealth/2.5))));
+        Health.Add(new MembershipFunction("NORMAL", (halfHealth-(halfHealth/3)), (halfHealth-(halfHealth/6)), halfHealth, halfHealth+100));
+        Health.Add(new MembershipFunction("HIGH", halfHealth, (halfHealth+(halfHealth/6)), fullHealth, fullHealth));
+        _myHealth = new LinguisticVariable("HEALTH", Health);
+
+        Mana.Add(new MembershipFunction("LOW", 0.0, 0.0, 20.0, 30.0));
+        Mana.Add(new MembershipFunction("NORMAL", 30.0, 45.0, 50.0, 55.0));
+        Mana.Add(new MembershipFunction("HIGH", 50.0, 55.0, 100.0, 100.0));
+        _myMana = new LinguisticVariable("MANA", Mana);
+
+        PlayerCondition.Add(new MembershipFunction("VERYCRITICAL", 0.0, 0.0, 1.0, 3.0));
+        PlayerCondition.Add(new MembershipFunction("CRITICAL", 2.5, 2.7, 2.8, 3.0));
+        PlayerCondition.Add(new MembershipFunction("UNSTABLE", 2.9, 3.1, 4.0, 4.5));
+        PlayerCondition.Add(new MembershipFunction("AVERAGE", 4.5, 5.0, 5.0, 6.0));
+        PlayerCondition.Add(new MembershipFunction("STABLE", 6.0, 6.5, 7.0, 7.5));
+        PlayerCondition.Add(new MembershipFunction("VERYSTABLE", 7.0, 7.5, 10, 10));
+        _myCondition = new LinguisticVariable("PLAYERCONDITION", PlayerCondition);
+    }
+
+    private void SetRules()
+    {
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS LOW) AND (MANA IS LOW) THEN PLAYERCONDITION IS VERYCRITICAL"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS NORMAL) AND (MANA IS LOW) THEN PLAYERCONDITION IS CRITICAL"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS LOW) AND (MANA IS NORMAL) THEN PLAYERCONDITION IS CRITICAL"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS HIGH) AND (MANA IS LOW) THEN PLAYERCONDITION IS UNSTABLE"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS LOW) AND (MANA IS HIGH) THEN PLAYERCONDITION IS UNSTABLE"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS NORMAL) AND (MANA IS NORMAL) THEN PLAYERCONDITION IS AVERAGE"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS HIGH) AND (MANA IS NORMAL) THEN PLAYERCONDITION IS STABLE"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS NORMAL) AND (MANA IS HIGH) THEN PLAYERCONDITION IS STABLE"));
+        FuzzyRules.Add(new FuzzyRule("IF (HEALTH IS HIGH) AND (MANA IS HIGH) THEN PLAYERCONDITION IS VERYSTABLE"));
+        FuzzyEngine.FuzzyRuleCollection = FuzzyRules;
+    }
+
+    private void SetFuzzyEngine()
+    {
+        FuzzyEngine.LinguisticVariableCollection.Add(_myHealth);
+        FuzzyEngine.LinguisticVariableCollection.Add(_myMana);
+        FuzzyEngine.LinguisticVariableCollection.Add(_myCondition);
+        FuzzyEngine.FuzzyRuleCollection = FuzzyRules;
     }
 
     public bool CheckInventory(string[] itemNames, int quantity, bool any = false, bool toInv = true)
